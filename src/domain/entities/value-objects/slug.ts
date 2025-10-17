7e7bb1d9ba911c2965ff1ed363984c6023486d0f
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-10-17 12:32:18 -0300

    feat: add Slug value object and tests for slug creation

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,26 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  /*
+   * Receives a text and converts it into a slug format.
+   * Example: "Hello World!" -> "hello-world"
+   * @param text {string}
+   */
+  static createFromText(text: string): Slug {
+    const slugText = text
+      .normalize("NFKD")
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, "-")
+      .replace(/[^\w-]+/g, "")
+      .replace(/_/g, "-")
+      .replace(/--+/g, "-")
+      .replace(/-$/g, "");
+
+    return new Slug(slugText);
+  }
+}
