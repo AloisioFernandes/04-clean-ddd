865e33db635fa84f10f76cfe186338d9274f91eb
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-10-21 07:25:29 -0300

    feat: update Answer and Question entities to use UniqueEntityID for ID management

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Entity } from "../../core/entities/entity.js";
+import type { UniqueEntityID } from "../../core/entities/unique-entity-id.js";
 import type { Slug } from "./value-objects/slug.js";
 
 interface QuestionProps {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
   title: string;
   content: string;
   slug: Slug;
-  authorId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
