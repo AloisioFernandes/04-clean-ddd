2f1f12990e85f1be369f3aace613237a90d5027e
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-10-27 07:05:18 -0300

    feat: implement CreateQuestion use case and QuestionsRepository interface

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "vitest";
+import type { Question } from "../../enterprise/entities/question.js";
+import type { QuestionsRepository } from "../repositories/questions-repository.js";
+import { CreateQuestionUseCase } from "./create-question.js";
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (question: Question) => {},
+};
+
+test("create a question", async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository);
+
+  const { question } = await createQuestion.execute({
+    authorId: "1",
+    title: "Nova pergunta",
+    content: "Conteúdo da pergunta",
+  });
+
+  expect(question.id).toBeTruthy();
+});
