5d821034b5a581e667a6625cdf668f07f30386a4
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-10-28 07:05:45 -0300

    feat: implement in-memory repositories for answers and questions, and update respective use case tests

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,20 +1,24 @@
-import { expect, test } from "vitest";
-import type { Question } from "../../enterprise/entities/question.js";
-import type { QuestionsRepository } from "../repositories/questions-repository.js";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository.js";
+import { describe, expect, it } from "vitest";
 import { CreateQuestionUseCase } from "./create-question.js";
 
-const fakeQuestionsRepository: QuestionsRepository = {
-  create: async (question: Question) => {},
-};
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: CreateQuestionUseCase;
 
-test("create a question", async () => {
-  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository);
-
-  const { question } = await createQuestion.execute({
-    authorId: "1",
-    title: "Nova pergunta",
-    content: "Conteúdo da pergunta",
+describe("Create Question", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository);
   });
 
-  expect(question.id).toBeTruthy();
+  it("should be able to create a question", async () => {
+    const { question } = await sut.execute({
+      authorId: "1",
+      title: "Nova pergunta",
+      content: "Conteúdo da pergunta",
+    });
+
+    expect(question.id).toBeTruthy();
+    expect(inMemoryQuestionsRepository?.items[0]?.id).toEqual(question.id);
+  });
 });
