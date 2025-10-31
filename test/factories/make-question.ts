b8069945074e67e265e29d0f83979659322fc56b
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-10-31 07:47:33 -0300

    feat: add @faker-js/faker dependency and update makeQuestion factory to use faker for generating random question data

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,16 +3,21 @@ import {
   Question,
   type QuestionProps,
 } from "@/domain/forum/enterprise/entities/question.js";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug.js";
+import { faker } from "@faker-js/faker";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: "Example Question",
-    content: "This is an example question content.",
-    slug: Slug.create("example-question"),
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id
+  );
 
   return question;
 }
