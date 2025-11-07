b3de977a5666a5c7601a2f91451072711f5504a0
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-11-07 06:53:17 -0300

    feat: implement FetchQuestionAnswers use case with pagination support and related tests

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,27 @@
+import type { Answer } from "../../enterprise/entities/answer.js";
+import type { AnswersRepository } from "../repositories/answers-repository.js";
+
+interface FetchQuestionAnswersUseCaseRequest {
+  page: number;
+  questionId: string;
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  answers: Answer[];
+}
+
+export class FetchQuestionAnswersUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page }
+    );
+
+    return { answers };
+  }
+}
