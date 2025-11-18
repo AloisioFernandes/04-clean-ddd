4df39d85050b013bf9cde9430153e6534c3dd05c
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-11-18 07:12:22 -0300

    feat: implement FetchAnswerComments use case and related in-memory repository methods

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -0,0 +1,27 @@
+import type { AnswerComment } from "../../enterprise/entities/answer-comment.js";
+import type { AnswerCommentsRepository } from "../repositories/answer-comments-repository.js";
+
+interface FetchAnswerCommentsUseCaseRequest {
+  page: number;
+  answerId: string;
+}
+
+interface FetchAnswerCommentsUseCaseResponse {
+  answerComments: AnswerComment[];
+}
+
+export class FetchAnswerCommentsUseCase {
+  constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
+
+  async execute({
+    answerId,
+    page,
+  }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
+    const answerComments =
+      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
+        page,
+      });
+
+    return { answerComments };
+  }
+}
