31892d59ed45a72df33def61ebf005c6675af440
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-11-24 06:50:18 -0300

    feat: update Either type tests for correct method usage and add error handling for DeleteAnswerComment use case

--- src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -2,14 +2,14 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id.js";
 import { makeAnswerComment } from "test/factories/make-answer-comment.js";
 import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository.js";
 import { DeleteAnswerCommentUseCase } from "./delete-answer-comment.js";
+import { NotAllowedError } from "./errors/not-allowed-error.js";
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
 let sut: DeleteAnswerCommentUseCase;
 
 describe("Delete Answer Comment", () => {
   beforeEach(() => {
-    inMemoryAnswerCommentsRepository =
-      new InMemoryAnswerCommentsRepository();
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
 
     sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository);
   });
@@ -34,11 +34,12 @@ describe("Delete Answer Comment", () => {
 
     await inMemoryAnswerCommentsRepository.create(answerComment);
 
-    expect(() => {
-      return sut.execute({
-        answerCommentId: answerComment.id.toString(),
-        authorId: "author-2",
-      });
-    }).rejects.toBeInstanceOf(Error);
+    const result = await sut.execute({
+      answerCommentId: answerComment.id.toString(),
+      authorId: "author-2",
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
   });
 });
