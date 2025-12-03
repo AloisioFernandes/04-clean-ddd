db432e9a8f61bc2394ecd2d40f58485c65b1760d
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-12-03 07:22:47 -0300

    feat: enhance CreateQuestionUseCase to handle attachments and update Question entity to store them

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id.js";
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository.js";
 import { describe, expect, it } from "vitest";
 import { CreateQuestionUseCase } from "./create-question.js";
@@ -16,11 +17,16 @@ describe("Create Question", () => {
       authorId: "1",
       title: "Nova pergunta",
       content: "Conteúdo da pergunta",
+      attachmentsIds: ["1", "2"],
     });
 
     expect(result.isRight()).toBe(true);
     expect(inMemoryQuestionsRepository?.items[0]).toEqual(
       result.value?.question
     );
+    expect(inMemoryQuestionsRepository?.items[0]?.attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID("1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID("2") }),
+    ]);
   });
 });
