1d01c884901948383730da3005bfb551ebc5e3c3
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-12-10 07:32:54 -0300

    feat: implement InMemoryQuestionAttachmentsRepository and update EditQuestionUseCase to manage question attachments

---
+++ src/domain/forum/application/repositories/in-memory-question-attachments-repository.ts
@@ -0,0 +1,16 @@
+import type { QuestionAttachment } from "../../enterprise/entities/question-attachment.js";
+import type { QuestionAttachmentsRepository } from "./question-attachments-repository.js";
+
+export class InMemoryQuestionAttachmentsRepository
+  implements QuestionAttachmentsRepository
+{
+  public items: QuestionAttachment[] = [];
+
+  async findManyByQuestionId(questionId: string) {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() === questionId
+    );
+
+    return questionAttachments;
+  }
+}
